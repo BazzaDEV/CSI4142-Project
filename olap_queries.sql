1174148e8788256fce2a826fddb85d71f22ebad0
Kian Bazarjani <bazarjani.kian@gmail.com>
2022-04-08 10:16:30 -0400

    Create olap_queries.sql

---
+++ olap_queries.sql
@@ -0,0 +1,19 @@
+----- Part 1: Standard OLAP Operations -----
+
+-- a) Drill Down, Roll Up (2 queries)
+
+-- b) Slice (1 query)
+
+-- c) Dice (2 queries)
+
+-- d) Combining OLAP Operations (4 queries)
+
+
+
+----- Part 2: Explorative Operations -----
+
+-- a) Iceberg Query
+
+-- b) Windowing Query
+
+-- c) Using the Window clause
